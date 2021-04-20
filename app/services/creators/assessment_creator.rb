4e2f70ed2794dc4e01fdb4e0bbccb7048eb1756b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-04-20 20:00:26 +0100

    AP-2176 WIP

--- app/services/creators/assessment_creator.rb
+++ app/services/creators/assessment_creator.rb
@@ -66,7 +66,7 @@ module Creators
         assessment.build_capital_summary
         assessment.build_gross_income_summary
         assessment.build_disposable_income_summary
-        Utilities::EligibilitiesCreator.call(assessment) if assessment.save
+        Creators::EligibilitiesCreator.call(assessment) if assessment.save
         assessment
       end
     end

d3531992dca7fe00953beb877bf07a1a12bced35
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2021-04-20 18:52:14 +0100

    AP-2176 WIP

--- app/services/collators/disposable_income_collator.rb
+++ app/services/collators/disposable_income_collator.rb
@@ -63,21 +63,5 @@ module Collators
     def disposable_income
       [0, total_gross_income - total_outgoings_and_allowances].max
     end
-
-    def lower_threshold
-      Threshold.value_for(:disposable_income_lower, at: assessment.submission_date)
-    end
-
-    def upper_threshold
-      assessment.matter_proceeding_type == 'domestic_abuse' ? no_upper_limit : standard_upper_limit
-    end
-
-    def standard_upper_limit
-      Threshold.value_for(:disposable_income_upper, at: assessment.submission_date)
-    end
-
-    def no_upper_limit
-      Threshold.value_for(:infinite_gross_income_upper)
-    end
   end
 end
